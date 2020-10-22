03235ac996523ca3562bb05bc175e9c7ef7a4e85
liunannan <15111390477@163.com>
2020-10-22 10:18:59 +0800

    completed GraphicalVerify.vue && modify README.md

--- src/views/tab/GraphicalVerify.vue
+++ src/views/tab/GraphicalVerify.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    图形验证码
+  <div class="gv-container">
+    <zx-verify
+      :w="350"
+      :slider-text="text"
+      :h="175"
+      @success="verifySuccess"
+      @fail="verifyFail">
+    </zx-verify>
   </div>
 </template>
 
 <script>
-  export default {}
+  import ZxVerify from 'zx-verify'
+  import 'zx-verify/dist/zx-verify.css'
+  export default {
+    data() {
+      return {
+        text: '向右滑动'
+      }
+    },
+    created() {},
+    mounted() {},
+    methods: {
+      verifySuccess() {
+        this.$message.closeAll()
+        this.$message.success('校验成功')
+      },
+      verifyFail() {
+        this.$message.closeAll()
+        this.$message.error('校验失败')
+      }
+    },
+    components: {
+      ZxVerify
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .gv-container
+    padding: 30px
 </style>
