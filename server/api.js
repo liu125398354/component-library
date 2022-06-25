43beb66c9768d0864408d3c5761d6a25d9361d13
liunannan <15111390477@163.com>
2022-06-25 16:24:12 +0800

    solve the problem that the system can not run because of some dependencies and modify the server file content format

--- server/api.js
+++ server/api.js
@@ -77,13 +77,13 @@ router.post('/api/apk/upload', (req, res) => {
   }
 
   let form = new formidable.IncomingForm()
-  form.multiples = true;
+  form.multiples = true
   form.uploadDir = './uploadFile'
   form.maxFileSize = 600 * 1024 * 1024
   form.parse(req, function(err, fields, files) {
     let allFiles = files.file
     // console.log('files--->', files)
-    console.log('文件信息---->',allFiles)
+    console.log('文件信息---->', allFiles)
     let poster = files.poster
     if (allFiles) {
       if (allFiles.length === undefined) {
@@ -95,6 +95,7 @@ router.post('/api/apk/upload', (req, res) => {
           let splitPath = value.path.split('_')
           let fileName = splitPath[splitPath.length - 1]
           let extName = '.' + splitName[splitName.length - 1]
+          // eslint-disable-next-line no-path-concat
           let newPath = __dirname + '/uploadFile/' + fileName + extName
           fs.rename(oldPath, newPath, function(err) {
             if (err) {
@@ -115,92 +116,91 @@ router.post('/api/apk/upload', (req, res) => {
 })
 
 router.post('/api/uploadChunk', async function(req, res) {
-
-  let form = new multiparty.Form(); //新建表单
-  //设置编辑
-  form.encoding = 'utf-8';
-  //设置文件存储路径
-  form.uploadDir = "./tempCopy"; // "Uploads/";
-  //设置单文件大小限制
+  let form = new multiparty.Form() // 新建表单
+  // 设置编辑
+  form.encoding = 'utf-8'
+  // 设置文件存储路径
+  form.uploadDir = './tempCopy' // "Uploads/";
+  // 设置单文件大小限制
   // form.maxFilesSize = 200 * 1024 * 1024;
-  /*form.parse表单解析函数，fields是生成数组用获传过参数，files是bolb文件名称和路径*/
+  /* form.parse表单解析函数，fields是生成数组用获传过参数，files是bolb文件名称和路径 */
   try {
     let [fields, files] = await new Promise((resolve, reject) => {
       form.parse(req, (err, fields, files) => {
-        if (err) reject('test err');
-        resolve([fields, files]);
+        // eslint-disable-next-line prefer-promise-reject-errors
+        if (err) reject('test err')
+        resolve([fields, files])
       })
     })
-    files = files['file'][0]; //获取bolb文件
-    let index = fields['index'][0]; //当前片数
-    let total = fields['total'][0]; //总片数
-    let name = fields['name'][0]; //文件名称
+    files = files.file[0] // 获取bolb文件
+    let index = fields.index[0] // 当前片数
+    let total = fields.total[0] // 总片数
+    let name = fields.name[0] // 文件名称
 
-    let url = './tempCopy/' + name + index; //临时bolb文件新名字
-    fs.renameSync(files.path, url); //修改临时文件名字
+    let url = './tempCopy/' + name + index // 临时bolb文件新名字
+    fs.renameSync(files.path, url) // 修改临时文件名字
 
-    let pathname = './uploadFile/' + name; // 上传文件存放位置和名称
+    let pathname = './uploadFile/' + name // 上传文件存放位置和名称
     if (index == total) { // 当最后一个分片上传成功，进行合并
       // 检查文件是存在，如果存在，重新设置名称
       let NonExist = await new Promise((resolve, reject) => {
         fs.access(pathname, fs.F_OK, (err) => {
-          resolve(err);
-        });
+          resolve(err)
+        })
       })
-      let md5 = fields['md5'][0]; //md5值
-      let info = fields['info'][0]
-      console.log('md5值--->',md5)
-      console.log('文件信息--->',info)
+      let md5 = fields.md5[0] // md5值
+      let info = fields.info[0]
+      console.log('md5值--->', md5)
+      console.log('文件信息--->', info)
       console.log(NonExist)
       if (!NonExist) {
-        let myDate = Date.now();
-        pathname = './uploadFile/' + myDate + name;
+        let myDate = Date.now()
+        pathname = './uploadFile/' + myDate + name
       }
-      //logs.info('上传文件：' + pathname);
-      console.log('上传文件：' + pathname);
-      /*进行合并文件，先创建可写流，再把所有BOLB文件读出来，
+      // logs.info('上传文件：' + pathname);
+      console.log('上传文件：' + pathname)
+      /* 进行合并文件，先创建可写流，再把所有BOLB文件读出来，
           流入可写流，生成文件
           fs.createWriteStream创建可写流
           aname是存放所有生成bolb文件路径数组:
           ['Uploads/3G.rar1','Uploads/3G.rar2',...]
       */
-      let writeStream = fs.createWriteStream(pathname);
-      let aname = [];
+      let writeStream = fs.createWriteStream(pathname)
+      let aname = []
       for (let i = 1; i <= total; i++) {
-        let url = './tempCopy/' + name + i;
+        let url = './tempCopy/' + name + i
         let data = await new Promise(function (resolve, reject) {
           fs.readFile(url, function (error, data) {
-            if (error) reject(error);
-            resolve(data);
-          });
-        });
-        //把数据写入流里
-        writeStream.write(data);
-        //删除生成临时bolb文件
-        fs.unlink(url, () => {});
+            if (error) reject(error)
+            resolve(data)
+          })
+        })
+        // 把数据写入流里
+        writeStream.write(data)
+        // 删除生成临时bolb文件
+        fs.unlink(url, () => {})
       }
-      writeStream.end();
-      //返回给客户端，上传成功
+      writeStream.end()
+      // 返回给客户端，上传成功
       let data = JSON.stringify({
-        'code': 0,
-        'index': index,
-        'msg': '上传成功'
-      });
-      res.send(data); //返回数据
-    } else { //还没有上传文件，请继续上传
+        code: 0,
+        index: index,
+        msg: '上传成功'
+      })
+      res.send(data) // 返回数据
+    } else { // 还没有上传文件，请继续上传
       let data = JSON.stringify({
-        'code': 1,
-        'index': index,
-        'msg': '继续上传'
-      });
-      res.send(data); //返回数据
+        code: 1,
+        index: index,
+        msg: '继续上传'
+      })
+      res.send(data) // 返回数据
     }
   } catch (e) {
-    //logs.info(e);
+    // logs.info(e);
     console.log(e)
-    res.send(e); //返回数据
+    res.send(e) // 返回数据
   }
 })
-
 
 module.exports = router
--- vue.config.js
+++ vue.config.js
@@ -40,12 +40,12 @@ module.exports = {
       errors: true
     },
     proxy: {
-      "/api": {
-        target: "http://192.168.0.132:3000/api",
+      '/api': {
+        target: 'http://192.168.0.12:3000/api',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "/"
+          '^/api': '/'
         }
       }
     } // string | Object
@@ -59,24 +59,24 @@ module.exports = {
         '@': resolve('src')
       }
     },
-    //或者
-    //警告 webpack 的性能提示
+    // 或者
+    // 警告 webpack 的性能提示
     performance: {
       hints: 'warning',
-      //入口起点的最大体积
+      // 入口起点的最大体积
       maxEntrypointSize: 50000000,
-      //生成文件的最大体积
+      // 生成文件的最大体积
       maxAssetSize: 30000000,
-      //只给出 js 文件的性能提示
+      // 只给出 js 文件的性能提示
       assetFilter: function(assetFilename) {
-        return assetFilename.endsWith('.js');
+        return assetFilename.endsWith('.js')
       }
     },
     plugins: [
       new CopyWebpackPlugin([
-        { from: 'node_modules/@liveqing/liveplayer/dist/component/crossdomain.xml'},
-        { from: 'node_modules/@liveqing/liveplayer/dist/component/liveplayer.swf'},
-        { from: 'node_modules/@liveqing/liveplayer/dist/component/liveplayer-lib.min.js', to: 'js/'}
+        { from: 'node_modules/@liveqing/liveplayer/dist/component/crossdomain.xml' },
+        { from: 'node_modules/@liveqing/liveplayer/dist/component/liveplayer.swf' },
+        { from: 'node_modules/@liveqing/liveplayer/dist/component/liveplayer-lib.min.js', to: 'js/' }
       ])
     ]
   },
