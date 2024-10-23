f151871b5618fc4eb2c844ddb6f33a85af7e663f
liunannan <15111390477@163.com>
2024-10-23 12:39:05 +0800

    modify router problem

--- src/config/permission.js
+++ src/config/permission.js
@@ -5,14 +5,23 @@
 import router from '@/router'
 import store from '@/store'
 
+let isAddRouter = false
 router.beforeEach(async (to, from, next) => {
   if (to.path === '/login') {
     next({ path: '/homepage' })
   } else {
-    let accessRoutes = []
-    accessRoutes = await store.dispatch('routes/setAllRoutes')
-    router.addRoutes(accessRoutes)
-    next()
+    if (!isAddRouter) {
+      let accessRoutes = []
+      accessRoutes = await store.dispatch('routes/setAllRoutes')
+      for (let i = 0; i < accessRoutes.length; i += 1) {
+        const element = accessRoutes[i]
+        router.addRoute(element)
+      }
+      isAddRouter = true
+      next({ ...to, replace: true })
+    } else {
+      next()
+    }
   }
 })
 
